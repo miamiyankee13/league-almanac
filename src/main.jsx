0e2f84fd54c121a434ff40fb4d4a9adccab7c2dc
Anthony <anthony.damico721@gmail.com>
2026-09-06 06:37:53 -0400

    Unify KPI card backgrounds

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./terminal.css";
+import "./kpiConsistency.css";
 
 let storedTheme = null;
 try {
